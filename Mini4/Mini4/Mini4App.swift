fb92455f8da6333140f9334e8e180b0be8c869b3
Barbara Argolo <92699920+BarbaraAM@users.noreply.github.com>
2023-03-22 17:01:12 -0300

    Commit inicial

---
+++ Mini4/Mini4/Mini4App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini4App.swift
+//  Mini4
+//
+//  Created by Barbara Argolo on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
