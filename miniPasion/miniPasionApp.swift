5185c122ff07e042ff38cf9ec537245a9a1b789f
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2023-03-22 13:57:10 -0300

    Initial Commit

---
+++ miniPasion/miniPasionApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniPasionApp.swift
+//  miniPasion
+//
+//  Created by Vinicius on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct miniPasionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
