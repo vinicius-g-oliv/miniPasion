fbabb9fb043fc7632a2ad4a3b432f5d063ef72bd
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2023-04-01 16:35:53 -0300

    Make custom view info

--- Mini4/View/InfoView.swift
+++ Mini4/View/InfoView.swift
@@ -16,50 +16,93 @@ struct InfoView: View {
         ZStack {
             GeometryReader(){ geo in
                 if show {
-                    // PopUp background color
-                    Color.black.opacity(show ? 0.3 : 0).edgesIgnoringSafeArea(.all)
                     // PopUp Window
-                    VStack(alignment: .center, spacing: 0) {
-                        
-                        Text(title)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 45, alignment: .center)
-                            .font(Font.system(size: 23, weight: .semibold))
-                            .foregroundColor(Color.white)
-                            .background(Color(#colorLiteral(red: 0.6196078431, green: 0.1098039216, blue: 0.2509803922, alpha: 1)))
-                        Button(action: {
-                            // Dismiss the PopUp
-                            withAnimation(.linear(duration: 0.3)) {
-                                show = false
-                            }
-                        }, label: {
-                            Text("Como Jogar")
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 54, alignment: .center)
-                                .foregroundColor(Color.white)
-                                .background(Color(#colorLiteral(red: 0.6196078431, green: 0.1098039216, blue: 0.2509803922, alpha: 1)))
-                                .font(Font.system(size: 23, weight: .semibold))
-                        }).buttonStyle(PlainButtonStyle())
-                        
-                        HStack {
-                            
+                    Color.black.opacity(show ? 0.3 : 0).edgesIgnoringSafeArea(.all)
+                    ZStack {
+                        Image("cardInfo").resizable()
+                        VStack {
                             Button(action: {
                                 // Dismiss the PopUp
                                 withAnimation(.linear(duration: 0.3)) {
                                     show = false
                                 }
                             }, label: {
-                                Text("Fechar")
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 54, alignment: .center)
-                                    .foregroundColor(Color.white)
-                                    .background(Color(#colorLiteral(red: 0.6196078431, green: 0.1098039216, blue: 0.2509803922, alpha: 1)))
-                                    .font(Font.system(size: 23, weight: .semibold))
-                            }).buttonStyle(PlainButtonStyle()).position(CGPoint(x: geo.size.height * 1, y: geo.size.height * 1))
+                                Image("close").resizable().frame(width: 30, height: 30)
+                            }).position(CGPoint(x: geo.size.width * 0.7, y: geo.size.height / 25))
+                            Text(title)
+                                .font(Font.system(size: 20, weight: .semibold))
+                                .foregroundColor(Color.white)
+                                .position(CGPoint(x: geo.size.width * 0.35, y: geo.size.height / 25))
+                            //MARK: CLose button
+                            
+                            
                         }
-                        .frame(maxWidth: 300)
-                        .background(Color(#colorLiteral(red: 0.737254902, green: 0.1294117647, blue: 0.2941176471, alpha: 1)))
-                    }
+                        
+                       
+                        
+                        //MARK: Informacoes title
+                       
+                        //MARK: COMO JOGAR
+                        Button(action: {
+                            // Dismiss the PopUp
+                            withAnimation(.linear(duration: 0.3)) {
+//                                show = false
+                            }
+                        }, label: {
+                            
+                            Image("questions").resizable().frame(width:30 ,height: 30).position(CGPoint(x: 50, y: 100))
+                            Text("Como Jogar").frame(width:120 ,height: 25).position(CGPoint(x: -20, y: 100))
+                            
+                            
+                                .foregroundColor(Color.white)
+                            
+                                .font(Font.system(size: 20, weight: .semibold))
+                            
+                            
+                        })
+                        
+                        //MARK: FORMAS
+                        Button(action: {
+                            // Dismiss the PopUp
+                            withAnimation(.linear(duration: 0.3)) {
+//                                show = false
+                            }
+                        }, label: {
+                            
+                            Image("formasInfo").resizable().frame(width:30 ,height: 30).position(CGPoint(x: 50, y: 150))
+                            Text("Formas").frame(width:90 ,height: 25).position(CGPoint(x: -40, y: 150))
+                            
+                            
+                                .foregroundColor(Color.white)
+                            
+                                .font(Font.system(size: 20, weight: .semibold))
+                            
+                            
+                        })
+                        
+                        
+                        //                        //MARK: SObre
+                        //
+                        Button(action: {
+                            // Dismiss the PopUp
+                            withAnimation(.linear(duration: 0.3)) {
+//                                show = false
+                            }
+                        }, label: {
+                            
+                            Image("info.circle").resizable().frame(width:30 ,height: 30).position(CGPoint(x: 50, y: 200))
+                            Text("Sobre").frame(width:90 ,height: 25).position(CGPoint(x: -50, y: 200))
+                            
+                            
+                                .foregroundColor(Color.white)
+                            
+                                .font(Font.system(size: 20, weight: .semibold))
+                            
+                            
+                        })
+                        
+                        
+                    }.frame(width: 300, height: 300).position(CGPoint(x: geo.size.width * 0.5, y: geo.size.height * 0.5))
                 }
             }
         }
