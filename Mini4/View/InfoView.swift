99825ac5c9be49fb422fe2b4244f63269be9957e
Barbara Argolo <92699920+BarbaraAM@users.noreply.github.com>
2023-04-12 15:35:10 -0300

    View Infos das Formas Geometricas

--- Mini4/View/InfoView.swift
+++ Mini4/View/InfoView.swift
@@ -13,64 +13,74 @@ struct InfoView: View {
     @Binding var show: Bool
     var body: some View {
         
-        ZStack {
-            
-            if show {
-                // PopUp Window
-                Color.black.opacity(show ? 0.3 : 0).edgesIgnoringSafeArea(.all)
-                ZStack {
-                    Image("cardInfo").resizable()
-                    
-                    VStack {
-                        GeometryReader(){ geo in
-                            HStack {
-                                Text(title).foregroundColor(.white).font(.system(size: 20)).bold()
-                            }.position(CGPoint(x: geo.size.width * 0.5, y: geo.size.height * 0.1))
-                            HStack {
-                                Button(action: {
-                                    withAnimation(.linear(duration: 0.3)) {
-                                        show = false
-                                    }
-                                }, label: {
-                                    Image("close").resizable().frame(width: 30, height: 30)
-                                })
-                            }.frame(width: 30,height: 30).position(CGPoint(x: geo.size.width * 0.9, y: geo.size.height * 0.1))
-                            
-                            HStack {
-                                Button(action: {
-                                    withAnimation(.linear(duration: 0.3)) {
-                                        print("A")
-                                    }
-                                }, label: {
-                                    Image("questions").resizable().frame(width: 30,height: 30)
-                                    Text("asa")
-                                })
+        NavigationView {
+            ZStack {
+                
+                if show {
+                    // PopUp Window
+                    Color.black.opacity(show ? 0.3 : 0).edgesIgnoringSafeArea(.all)
+                    ZStack {
+                        Image("cardInfo").resizable()
+                        
+                        VStack {
+                            GeometryReader(){ geo in
+                                HStack {
+                                    Text(title).foregroundColor(.white).font(.system(size: 20)).bold()
+                                }.position(CGPoint(x: geo.size.width * 0.5, y: geo.size.height * 0.1))
+                                HStack {
+                                    Button(action: {
+                                        withAnimation(.linear(duration: 0.3)) {
+                                            show = false
+                                        }
+                                    }, label: {
+                                        Image("close").resizable().frame(width: 30, height: 30)
+                                    })
+                                }.frame(width: 30,height: 30).position(CGPoint(x: geo.size.width * 0.9, y: geo.size.height * 0.1))
                                 
-                            }.position(CGPoint(x: geo.size.width * 0.3, y:  geo.size.height * 0.3))
-                            HStack {
-                                Button(action: {
-                                    withAnimation(.linear(duration: 0.3)) {
-                                        print("B")
-                                    }
-                                }, label: {
+                                HStack {
+                                    Button(action: {
+                                        withAnimation(.linear(duration: 0.3)) {
+                                            print("A")
+                                        }
+                                    }, label: {
+                                        Image("questions").resizable().frame(width: 30,height: 30)
+                                        Text("asa")
+                                    })
                                     
-                                    Image("questions").resizable().frame(width: 30,height: 30)
-                                    Text("asa")
-                                })
-                            }.position(CGPoint(x: geo.size.width * 0.3, y:  geo.size.height * 0.5))
-                            HStack {
-                                Button(action: {
-                                    withAnimation(.linear(duration: 0.3)) {
-                                        print("C")
+                                }.position(CGPoint(x: geo.size.width * 0.3, y:  geo.size.height * 0.3))
+                                //
+                                NavigationLink(destination: InitialFormsInfoView()) {
+                                    HStack {
+                                        Text("Formas Geométricas").bold().font(Font.system(size: 20)).foregroundColor(.white)
                                     }
-                                }, label: {
-                                    Image("questions").resizable().frame(width: 30,height: 30)
-                                    Text("asa")
-                                })
-                            }.position(CGPoint(x: geo.size.width * 0.3, y:  geo.size.height * 0.7))
-                        }
+                                }.position(CGPoint(x: geo.size.width * 0.5, y: geo.size.height * 0.5))
+//                                HStack {
+//                                    Button(action: {
+//
+//                                        withAnimation(.linear(duration: 0.3)) {
+//                                            print("B")
+//                                        }
+//                                    }, label: {
+//
+//                                        Image("questions").resizable().frame(width: 30,height: 30)
+//                                        Text("asa")
+//                                    })
+//                                }.position(CGPoint(x: geo.size.width * 0.3, y:  geo.size.height * 0.5))
+                              //
+                                HStack {
+                                    Button(action: {
+                                        withAnimation(.linear(duration: 0.3)) {
+                                            print("C")
+                                        }
+                                    }, label: {
+                                        Image("questions").resizable().frame(width: 30,height: 30)
+                                        Text("asa")
+                                    })
+                                }.position(CGPoint(x: geo.size.width * 0.3, y:  geo.size.height * 0.7))
+                            }
+                        }.frame(width: 300, height: 300)
                     }.frame(width: 300, height: 300)
-                }.frame(width: 300, height: 300)
+                }
             }
         }.navigationBarBackButtonHidden().navigationViewStyle(StackNavigationViewStyle())
     }
---
+++ Mini4/View/InitialFormsInfoView.swift
@@ -0,0 +1,62 @@
+//
+//  InitialFormsInfoView.swift
+//  Mini4
+//
+//  Created by Barbara Argolo on 11/04/23.
+//
+
+import SwiftUI
+
+
+
+struct InitialFormsInfoView: View {
+    @State private var showPopUp: Bool = false
+    @State var count = 0
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            ZStack {
+                Image("cardInfo").resizable()
+                    .frame(width: 300, height: 300)
+                VStack {
+                    Image("formasInfos").resizable()
+                        .frame(width: 99.6, height: 99.5)
+                    
+                    
+                    Text("Como Jogar")
+                        .font(Font.system(size: 17, weight: .semibold))
+                        .foregroundColor(Color.white)
+                        .frame(height: 30)
+                    
+                    Text("Essas são as formas geométricas que usamos para o jogo!").multilineTextAlignment(.center)
+                        .font(Font.system(size: 15, weight: .semibold))
+                        .foregroundColor(Color.white)
+                        .frame(width: 250, height: 40)
+                }
+                Button(action: {
+                    count += 1
+                    if count > 1 {
+                        showPopUp.toggle()
+                    }
+                    
+                }, label: {
+                    Image("botaoSeguinte").resizable()
+                    
+                }).frame(width: 13, height: 25).position(x: geo.size.width * 0.95, y: geo.size.height * 0.5)
+                
+            }.position(x: geo.size.width * 0.5, y: geo.size.height * 0.5)
+//            FormsInfoView(show: $showPopUp)
+        }
+        
+        .background(.purple)
+    }
+}
+    
+
+
+//struct InitialFormsInfoView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        InitialFormsInfoView()
+//    }
+//}
