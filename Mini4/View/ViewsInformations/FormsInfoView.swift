07c36f0db5bb7b16570a4934282a2b33f1bb6168
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2023-04-14 15:10:34 -0300

    Revert "Revert "Merge remote-tracking branch 'origin/Correções-Finais'""
    This reverts commit 1d75fb1dc26d7216c2cd11004c351459c0eee889.

--- Mini4/View/ViewsInformations/FormsInfoView.swift
+++ Mini4/View/ViewsInformations/FormsInfoView.swift
@@ -4,11 +4,10 @@ struct FormsInfoView: View {
     @State var whiteForms = GeometryForm.forms
     @State var count = 0
     @Binding var show: Bool
-
     
     var body: some View {
         
-            
+        
         GeometryReader { geo in
             if show {
                 // PopUp Window
@@ -18,24 +17,42 @@ struct FormsInfoView: View {
                     Image("cardInfo").resizable()
                         .frame(width: 300, height: 300)
                     VStack {
+                        
                         Image(whiteForms[count].image).resizable()
                             .frame(width: 136, height: 136)
                         Text(whiteForms[count].name)
+                            .font(Font.custom("Jost-Medium", size: 24))
                             .foregroundColor(.white)
+                            .frame(width: 280)
+                            .lineLimit(2)
+                            .multilineTextAlignment(.center)
+                            
+                            
+                        
                     }
-                    //increment
+                    VStack {
+                        Button(action: {
+                            // Dismiss the PopUp
+                            withAnimation(.linear(duration: 0.3)) {
+                                show = false // usar outra lógica aqui
+                            }
+                        }, label: {
+                            Image("voltar4x").resizable().frame(width: 30, height: 30)
+                        }).position(CGPoint(x: geo.size.width * -0.27, y: geo.size.height * -0.13))
+                            .frame(width: 30, height: 30)
+                    }
+         
                     Button(action: {
-                       
+                        
                         if count >= 0 &&  count < 7 {
                             count += 1
                         }
-                      
                         
                     }, label: {
                         Image("botaoseguinte").resizable()
                         
                     }).frame(width: 13, height: 25).position(x: geo.size.width * 0.95, y: geo.size.height * 0.5)
-                        //decrement
+                    //decrement
                     Button(action: {
                         
                         
@@ -52,9 +69,7 @@ struct FormsInfoView: View {
                     
                 }
                 .position(x: geo.size.width * 0.5, y: geo.size.height * 0.5)
-                
             }
         }
-        
     }
 }
